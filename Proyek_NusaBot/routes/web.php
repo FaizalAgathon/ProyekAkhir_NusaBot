fc1376d5862cbe4dec86ddffbb0b666240e59bb9
FaizalAgathon <faizalagathon06@gmail.com>
2023-04-05 22:47:20 +0700

    + tamplate

--- Proyek_NusaBot/routes/web.php
+++ Proyek_NusaBot/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+Route::middleware(['first', 'second'])->group(function () {
+  Route::get('/', function () {
+    // Uses first & second middleware...
+  });
+
+  Route::get('/user/profile', function () {
+    // Uses first & second middleware...
+  });
+});
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
